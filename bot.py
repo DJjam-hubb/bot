7875ccb45b2d93978cab36e2eaaa79a0af1882fa
Danil <60926489+DJjam-hubb@users.noreply.github.com>
2020-02-12 18:40:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,10 @@ import telebot
 import requests
 from datetime import datetime
 
-
-
 bot = telebot.TeleBot('1050229554:AAFPkDrue8DnVa3T1ir-nCv3xg3Nq4ww-jA')
 
-@bot.message_handler(content_types=['text'])
 
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/start":
         bot.send_message(message.from_user.id, "Привет,я Шпипс отправь мне любое сообщение и получи расписание.")
@@ -23,10 +21,10 @@ def get_text_messages(message):
         key_3 = telebot.types.InlineKeyboardButton(text='До ЕГЭ...', callback_data='ege')
         keyboard.add(key_3)
         bot.send_message(message.from_user.id, text="Привет :)", reply_markup=keyboard)
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-
-
     if call.data == "rasp":
 
         res = requests.get('http://brgi.ucoz.ru/index/raspisanie_urokov/0-65', timeout=30.0)
@@ -44,17 +42,18 @@ def callback_worker(call):
         l2 = page.index('10:10')
         l3 = page.index('11:15')
         l4 = page.index('12:20')
-        l5 = page.index('13:15') 
+        l5 = page.index('13:15')
         l6 = page.index('14:15')
         l7 = page.index('15:10')
+        l8 = page.index('16:05')
 
-
-        page1 = page[l1:l2].split("<td class=T1 style=';border-top:2 solid #707070;text-align:left;border-top:2 solid #707070")
+        page1 = page[l1:l2].split(
+            "<td class=T1 style=';border-top:2 solid #707070;text-align:left;border-top:2 solid #707070")
         page1 = page1[21].split(">")
 
         page1 = "".join(page1)
         page1 = [word for word in page1 if 1039 < ord(word[0])]
-        page1 = "1) " + "".join(page1)
+        page1 = "".join(page1)
         if "Б" in page1[5:]:
             page1 = page1.replace("Б", " / Б")
         elif "Х" in page1[5:]:
@@ -63,13 +62,14 @@ def callback_worker(call):
             page1 = page1.replace("И", " / И")
         elif "Ф" in page1[5:]:
             page1 = page1.replace("Ф", " / Ф")
-
+        if len(page1) != 0:
+            page1 = "1) " + page1
 
         page2 = page[l2:l3].split(" <td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page2 = page2[21].split(">")
         page2 = "".join(page2)
         page2 = [word for word in page2 if 1039 < ord(word[0])]
-        page2 = "2) " + "".join(page2)
+        page2 = "".join(page2)
         if "Б" in page2[5:]:
             page2 = page2.replace("Б", " / Б")
         elif "Х" in page2[5:]:
@@ -78,13 +78,14 @@ def callback_worker(call):
             page2 = page2.replace("И", " / И")
         elif "Ф" in page2[5:]:
             page2 = page2.replace("Ф", " / Ф")
-
+        if len(page2) != 0:
+            page2 = "2) " + page2
 
         page3 = page[l3:l4].split("<td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page3 = page3[21]
         page3 = "".join(page3)
         page3 = [word for word in page3 if 1039 < ord(word[0])]
-        page3 = "3) " + "".join(page3)
+        page3 = "".join(page3)
         if "Б" in page3[5:]:
             page3 = page3.replace("Б", " / Б")
         elif "Х" in page3[5:]:
@@ -93,12 +94,13 @@ def callback_worker(call):
             page3 = page3.replace("И", " / И")
         elif "Ф" in page3[5:]:
             page3 = page3.replace("Ф", " / Ф")
-
+        if len(page3) != 0:
+            page3 = "3) " + page3
 
         page4 = page[l4:l5].split("<td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page4 = page4[21]
         page4 = [word for word in page4 if 1039 < ord(word[0])]
-        page4 = "4) " + "".join(page4)
+        page4 = "".join(page4)
         if "Б" in page4[5:]:
             page4 = page4.replace("Б", " / Б")
         elif "Х" in page4[5:]:
@@ -107,12 +109,13 @@ def callback_worker(call):
             page4 = page4.replace("И", " / И")
         elif "Ф" in page4[5:]:
             page4 = page4.replace("Ф", " / Ф")
-
+        if len(page4) != 0:
+            page4 = "4) " + page4
 
         page5 = page[l5:l6].split(" <td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page5 = page5[21]
         page5 = [word for word in page5 if 1039 < ord(word[0])]
-        page5 = "5) " + "".join(page5)
+        page5 = "".join(page5)
         if "Б" in page5[5:]:
             page5 = page5.replace("Б", " / Б")
         elif "Х" in page5[5:]:
@@ -121,7 +124,8 @@ def callback_worker(call):
             page5 = page5.replace("И", " / И")
         elif "Ф" in page5[5:]:
             page5 = page5.replace("Ф", " / Ф")
-
+        if len(page5) != 0:
+            page5 = "5) " + page5
 
         page6 = page[l6:l7].split("<td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page6 = page6[21]
@@ -135,8 +139,10 @@ def callback_worker(call):
             page6 = page6.replace("И", " / И")
         elif "Ф" in page6[5:]:
             page6 = page6.replace("Ф", " / Ф")
+        if len(page6) != 0:
+            page6 = "6) " + page6
 
-        page7 = page[l7:-1].split("<td class=T1 style=';text-align:left'>")
+        page7 = page[l7:l8].split("<td class=T1 style=';text-align:left'>")
         page7 = page7[21]
         page7 = [word for word in page7 if 1039 < ord(word[0])]
         page7 = "".join(page7)
@@ -151,6 +157,23 @@ def callback_worker(call):
 
         if len(page7) != 0:
             page7 = "7) " + page7
+
+        page8 = page[l8:-1].split("<td class=T1 style=';text-align:left'>")
+        page8 = page8[21]
+        page8 = [word for word in page8 if 1039 < ord(word[0])]
+        page8 = "".join(page8)
+        if "Б" in page8[5:]:
+            page8 = page8.replace("Б", " / Б")
+        elif "Х" in page8[5:]:
+            page8 = page8.replace("Х", " / Х")
+        elif "И" in page8[5:]:
+            page8 = page8.replace("И", " / И")
+        elif "Ф" in page8[5:]:
+            page8 = page8.replace("Ф", " / Ф")
+
+        if len(page8) != 0:
+            page8 = "8) " + page8
+
         a = "Расписание 11а на " + date + "\n" + "\n"
         a += page1 + "\n" + page2 + "\n" + page3 + "\n" + page4 + "\n" + page5 + "\n" + page6 + "\n" + page7 + "\n"
 
@@ -179,7 +202,6 @@ def callback_worker(call):
         result = requests.get(x, timeout=30.0)
         result.encoding = 'cp1251'
         page = result.text
-
         l1 = page.index('09:15')
         l2 = page.index('10:10')
         l3 = page.index('11:15')
@@ -187,6 +209,7 @@ def callback_worker(call):
         l5 = page.index('13:15')
         l6 = page.index('14:15')
         l7 = page.index('15:10')
+        l8 = page.index('16:05')
 
         page1 = page[l1:l2].split(
             "<td class=T1 style=';border-top:2 solid #707070;text-align:left;border-top:2 solid #707070")
@@ -194,7 +217,7 @@ def callback_worker(call):
 
         page1 = "".join(page1)
         page1 = [word for word in page1 if 1039 < ord(word[0])]
-        page1 = "1) " + "".join(page1)
+        page1 = "".join(page1)
         if "Б" in page1[5:]:
             page1 = page1.replace("Б", " / Б")
         elif "Х" in page1[5:]:
@@ -203,12 +226,14 @@ def callback_worker(call):
             page1 = page1.replace("И", " / И")
         elif "Ф" in page1[5:]:
             page1 = page1.replace("Ф", " / Ф")
+        if len(page1) != 0:
+            page1 = "1) " + page1
 
         page2 = page[l2:l3].split(" <td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page2 = page2[22].split(">")
         page2 = "".join(page2)
         page2 = [word for word in page2 if 1039 < ord(word[0])]
-        page2 = "2) " + "".join(page2)
+        page2 = "".join(page2)
         if "Б" in page2[5:]:
             page2 = page2.replace("Б", " / Б")
         elif "Х" in page2[5:]:
@@ -217,12 +242,14 @@ def callback_worker(call):
             page2 = page2.replace("И", " / И")
         elif "Ф" in page2[5:]:
             page2 = page2.replace("Ф", " / Ф")
+        if len(page2) != 0:
+            page2 = "2) " + page2
 
         page3 = page[l3:l4].split("<td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page3 = page3[22]
         page3 = "".join(page3)
         page3 = [word for word in page3 if 1039 < ord(word[0])]
-        page3 = "3) " + "".join(page3)
+        page3 = "".join(page3)
         if "Б" in page3[5:]:
             page3 = page3.replace("Б", " / Б")
         elif "Х" in page3[5:]:
@@ -231,11 +258,13 @@ def callback_worker(call):
             page3 = page3.replace("И", " / И")
         elif "Ф" in page3[5:]:
             page3 = page3.replace("Ф", " / Ф")
+        if len(page3) != 0:
+            page3 = "3) " + page3
 
         page4 = page[l4:l5].split("<td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page4 = page4[22]
         page4 = [word for word in page4 if 1039 < ord(word[0])]
-        page4 = "4) " + "".join(page4)
+        page4 = "".join(page4)
         if "Б" in page4[5:]:
             page4 = page4.replace("Б", " / Б")
         elif "Х" in page4[5:]:
@@ -244,11 +273,13 @@ def callback_worker(call):
             page4 = page4.replace("И", " / И")
         elif "Ф" in page4[5:]:
             page4 = page4.replace("Ф", " / Ф")
+        if len(page4) != 0:
+            page4 = "4) " + page4
 
         page5 = page[l5:l6].split(" <td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page5 = page5[22]
         page5 = [word for word in page5 if 1039 < ord(word[0])]
-        page5 = "5) " + "".join(page5)
+        page5 = "".join(page5)
         if "Б" in page5[5:]:
             page5 = page5.replace("Б", " / Б")
         elif "Х" in page5[5:]:
@@ -257,6 +288,8 @@ def callback_worker(call):
             page5 = page5.replace("И", " / И")
         elif "Ф" in page5[5:]:
             page5 = page5.replace("Ф", " / Ф")
+        if len(page5) != 0:
+            page5 = "5) " + page5
 
         page6 = page[l6:l7].split("<td class=T1 style=';text-align:left'><table cellspacing=0 border=0")
         page6 = page6[22]
@@ -270,8 +303,10 @@ def callback_worker(call):
             page6 = page6.replace("И", " / И")
         elif "Ф" in page6[5:]:
             page6 = page6.replace("Ф", " / Ф")
+        if len(page6) != 0:
+            page6 = "6) " + page6
 
-        page7 = page[l7:-1].split("<td class=T1 style=';text-align:left'>")
+        page7 = page[l7:l8].split("<td class=T1 style=';text-align:left'>")
         page7 = page7[22]
         page7 = [word for word in page7 if 1039 < ord(word[0])]
         page7 = "".join(page7)
@@ -286,7 +321,24 @@ def callback_worker(call):
 
         if len(page7) != 0:
             page7 = "7) " + page7
-        a = "Расписание 11б на " + date + "\n" + "\n"
+
+        page8 = page[l8:-1].split("<td class=T1 style=';text-align:left'>")
+        page8 = page8[22]
+        page8 = [word for word in page8 if 1039 < ord(word[0])]
+        page8 = "".join(page8)
+        if "Б" in page8[5:]:
+            page8 = page8.replace("Б", " / Б")
+        elif "Х" in page8[5:]:
+            page8 = page8.replace("Х", " / Х")
+        elif "И" in page8[5:]:
+            page8 = page8.replace("И", " / И")
+        elif "Ф" in page8[5:]:
+            page8 = page8.replace("Ф", " / Ф")
+
+        if len(page8) != 0:
+            page8 = "8) " + page8
+
+        a = "Расписание 11а на " + date + "\n" + "\n"
         a += page1 + "\n" + page2 + "\n" + page3 + "\n" + page4 + "\n" + page5 + "\n" + page6 + "\n" + page7 + "\n"
 
         if "язык" in a:
@@ -301,7 +353,7 @@ def callback_worker(call):
             a = a.replace("Алгебраиначалаанализа", "Алгебра")
 
         bot.send_message(call.from_user.id, a)
-        
+
 
     if call.data == "ege":
         q = str(datetime.now())
@@ -309,13 +361,7 @@ def callback_worker(call):
         q = q[-2:] + "." + q[-5:-3] + "." + "2020"
         d1 = datetime.strptime(q, "%d.%m.%Y")
         d2 = datetime.strptime("25.05.2020", "%d.%m.%Y")
-        q = str((d2-d1).days) + " дн. (до 25 мая)"
+        q = str((d2 - d1).days) + " дн. (до 25 мая)"
         bot.send_message(call.from_user.id, q)
-
-
-
-
-
-
 
 bot.polling(none_stop=True)
